d6184192a99b1baa5b32828ab3bdb4120f051a1c
alejandra cantu <alejandra.cantu13@gmail.com>
2020-02-24 19:40:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -74,9 +74,10 @@ def self.find_by_id(id)
   SQL
 
   DB[:conn].execute(sql,id).map do |row|
-    self.new_from_db(row)
+    dog_from_db = self.new_from_db(row)
+  
 
-  end
+  end.dog_from_db
 end
 
 def self.find_or_create_by(name:, breed:)
