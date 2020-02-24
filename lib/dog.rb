b7bde118344b5b8143e952a55d7969b59ddedbae
alejandra cantu <alejandra.cantu13@gmail.com>
2020-02-24 19:37:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -87,7 +87,7 @@ WHERE name = ? AND breed = ?", name, breed
 )
 if !dog.empty?
   dog_attributes = dog[0]
-  dog = Dog.new(dog_attributes[0], dog_attributes[1], dog_attributes[2])
+  dog = Dog.new(id: dog_attributes[0], name: dog_attributes[1], breed: dog_attributes[2])
   dog
 else
   dog = self.create(name: name, breed: breed)
